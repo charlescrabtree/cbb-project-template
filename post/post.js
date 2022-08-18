88dd135ecb8373924ef7628a2542108cdc542a1c
charlescrabtree <charles.crabtree.horn@gmail.com>
2022-08-17 20:52:35 -0700

    added detail to post /index.html and started post.js

---
+++ post/post.js
@@ -0,0 +1,4 @@
+import { displayPost, checkAuth } from '../fetch-utils.js';
+import { deletePost } from '../fetch-utils.js';
+
+const displayPostContainer = document.querySelector('.post-detail-container');
