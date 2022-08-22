8d292c311a23dbbe9848ee7e1bef991c644d5758
charlescrabtree <charles.crabtree.horn@gmail.com>
2022-08-22 13:20:41 -0700

    fixed missing html in main index.html, added javascript to users.js

--- users/users.js
+++ users/users.js
@@ -1,2 +1,21 @@
 import { getProfiles, checkAuth, signOutUser } from '../fetch-utils.js';
 import { renderProfile } from '../render-utils.js';
+
+const usersContainer = document.getElementById('users-container');
+
+const signOutLink = document.getElementById('sign-out-link');
+signOutLink.addEventListener('click', signOutUser);
+
+checkAuth();
+
+async function displayProfiles() {
+    const users = await getProfiles();
+
+    for (let user of users) {
+        const renderedProfile = renderProfile(user);
+        renderedProfile.classList.add('post-it');
+
+        usersContainer.append(renderedProfile);
+    }
+}
+displayProfiles();
